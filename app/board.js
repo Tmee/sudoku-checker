847c08fe2076a29df6bf6aaa53bc9621a32ecfa7
Timothy Mee <tjmee90@gmail.com>
2015-02-12 13:37:09 -0700

    removed the highlight method, its not neccessary now... maybe an extention.  the board is now hooked up to write in an input, but no saving yet

--- app/board.js
+++ app/board.js
@@ -7,16 +7,18 @@ $(function() {
 
     $(document.body).bind('click', function() {
         if ($selected.length > 0) {
-            closeCellInput($selected);
+          closeCellInput($selected);
         }
     });
 
-    $board.on('.cell.empty', 'click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
+    $boardCells.click( function() {
+      $(this).html('<input type="text"/>').find('input').focus();
+    })
 
+    $board.on('.cell.empty', 'click', function(e) {
         if ($selected.length > 0) {
-            closeCellInput($selected);
+          console.log($selected);
+          closeCellInput($selected);
         }
 
         $selected = $(this);
@@ -30,9 +32,6 @@ $(function() {
         var $this = $(this);
         var $cell = $this.closest('.cell');
 
-        e.preventDefault();
-        e.stopPropagation();
-
         // ENTER, ESC
         if (e.keyCode == 13 || e.keyCode == 27) {
             closeCellInput($cell);
@@ -40,15 +39,12 @@ $(function() {
             return;
         }
         // digits between 1-9
-        else if (e.keyCode >= 49 && e.keyCode <= 57) {
+        if (e.keyCode >= 49 && e.keyCode <= 57) {
             var number = e.keyCode-48;
             $cell.find('input').val(number);
         }
 
         return;
-    });
-
-    $board.on('.cell.solved', 'click', handleHighlightTrigger);
     });
 
     function editCell() {
@@ -78,21 +74,5 @@ $(function() {
 
         $cell.empty().removeClass('empty').removeClass('solved').attr('style', null);
         $selected = $([]);
-    }
-
-    function handleHighlightTrigger(e) {
-        e.preventDefault();
-
-        if ($selected.length > 0) {
-            closeCellInput($selected);
-        }
-
-        var $this  = $(this);
-        var number = $this.text();
-
-        $selected.empty();
-        $selected = $([]);
-
-        return;
     }
 });
