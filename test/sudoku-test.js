8357517eaf571d522f0722cd02ebfa65c0dc2604
David Bayendor <dbayendor@gmail.com>
2015-02-18 13:53:34 -0700

    Fix library module load, remove duplicate test code

--- test/sudoku-test.js
+++ test/sudoku-test.js
@@ -22,15 +22,16 @@ module ('Sudoku Puzzle Tests');
     [3,0,0,0,9,0,0,0,0]
   ];
 
-test('make board into an array', function() {
+  var expectedPositions = [
+    [0,0],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[1,0],[1,1],
+    [1,2],[1,5],[2,0],[2,1],[2,2],[2,6],[2,7],[2,8],[3,0],
+    [3,1],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[4,4],[4,5],
+    [4,7],[4,8],[5,1],[5,3],[5,4],[5,5],[5,8],[6,1],[6,2],
+    [6,4],[6,6],[6,8],[7,1],[7,3],[7,4],[7,5],[7,8],[8,1],
+    [8,2],[8,3],[8,5],[8,6],[8,7],[8,8]
+  ];
 
-  function parseBoard(board) {
-      return board.split('\n').map(function(row) {
-        return row.split('').map(function(num) {
-          return +num;
-        });
-      });
-    };
+test('make board into an array', function() {
 
   var parsedBoard = parseBoard(board);
 
@@ -42,28 +43,6 @@ test('make board into an array', function() {
 
 test('should save all of the empty positions of board', function() {
 
-  var expectedPositions = [
-    [0,0],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[1,0],[1,1],
-    [1,2],[1,5],[2,0],[2,1],[2,2],[2,6],[2,7],[2,8],[3,0],
-    [3,1],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[4,4],[4,5],
-    [4,7],[4,8],[5,1],[5,3],[5,4],[5,5],[5,8],[6,1],[6,2],
-    [6,4],[6,6],[6,8],[7,1],[7,3],[7,4],[7,5],[7,8],[8,1],
-    [8,2],[8,3],[8,5],[8,6],[8,7],[8,8]
-  ];
-
-  function saveEmptyPositions(board) {
-    var emptyPositions = [];
-
-    for(var i = 0; i < board.length; i++) {
-      for(var j = 0; j < board[i].length; j++) {
-        if(board[i][j] === 0) {
-          emptyPositions.push([i, j]);
-        }
-      }
-    }
-    return emptyPositions;
-  };
-
   var emptyPositions = saveEmptyPositions(expectedBoard);
 
   deepEqual(51, emptyPositions.length);
@@ -73,15 +52,6 @@ test('should save all of the empty positions of board', function() {
 
 test('check row for values', function(assert) {
 
-  function checkRow(board, row, value) {
-    for(var i = 0; i < board[row].length; i++) {
-      if(board[row][i] === value) {
-        return i;
-      }
-    }
-    return true;
-  };
-
   assert.ok(checkRow(expectedBoard, 0, 2));
   assert.equal(1, checkRow(expectedBoard, 0, 9));
 
@@ -89,44 +59,12 @@ test('check row for values', function(assert) {
 
 test('check column for values', function(assert) {
 
-  function checkColumn(board, column, value) {
-    for(var i = 0; i < board.length; i++) {
-      if(board[i][column] === value) {
-        return i;
-      }
-    }
-    return true;
-  };
-
   assert.ok(checkColumn(expectedBoard, 0, 9));
   assert.equal(4, checkColumn(expectedBoard, 0, 5));
 
 });
 
 test('check 3x3 values do not match', function(assert) {
-
-  function checkSquare(board, column, row, value) {
-    var columnCorner = 0;
-    var rowCorner = 0;
-    var squareSize = 3;
-
-    while(column >= columnCorner + squareSize) {
-      columnCorner += squareSize;
-    }
-
-    while(row >= rowCorner + squareSize) {
-      rowCorner += squareSize;
-    }
-
-    for(var i = rowCorner; i < rowCorner + squareSize; i++) {
-      for(var j = columnCorner; j < columnCorner + squareSize; j++) {
-        if(board[i][j] === value) {
-          return [i, j];
-        }
-      }
-    }
-    return true;
-  };
 
   assert.ok(checkSquare(expectedBoard, 2, 2, 1));
   assert.ok(checkSquare(expectedBoard, 7, 7, 9));
