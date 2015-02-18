48c30896c5fa1835d8d514a134b09f167caa3c2b
Timothy Mee <tjmee90@gmail.com>
2015-02-18 15:34:12 -0700

    organizing tree, this code is not functional

--- app/board.js
+++ app/board.js
@@ -2,23 +2,76 @@ $(function() {
     var undefined;
     var $game            = $('.game');
     var $board           = $game.find('.board');
-    var $emptyBoardCells = $board.find('.cell.empty');
     var $selected        = $([]);
+    var expectedBoard = [[0,9,0,0,0,0,0,0,6],
+                        [0,0,0,9,6,0,4,8,5],
+                        [0,0,0,5,8,1,0,0,0],
+                        [0,0,4,0,0,0,0,0,0],
+                        [5,1,7,2,0,0,9,0,0],
+                        [6,0,2,0,0,0,3,7,0],
+                        [1,0,0,8,0,4,0,2,0],
+                        [7,0,6,0,0,0,8,1,0],
+                        [3,0,0,0,9,0,0,0,0]
+                      ];
 
-    $(document.body).bind('click', function() {
-        if ($selected.length > 0) {
-          closeCellInput($selected);
+
+    // Create the board
+    $('.row').each(function (rowIndex, row) {
+      $(this).find('.cell').each(function (cellIndex, cell) {
+        var cellValue = expectedBoard[rowIndex][cellIndex];
+        if (cellValue) {
+          $(this).text(cellValue);
+          $(this).addClass('set');
         }
+        else {
+          $(this).addClass('empty');
+        }
+      });
     });
+    //  finish board
 
+    function makeAnswersArray(board) {
+      var answerArray = [
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]
+      ];
+      $('.row').each(function (rowIndex, row) {
+        $(this).find('.cell').each(function (cellIndex, cell) {
+          var cellValue = parseInt($(this).text());
+          if (!cellValue) { cellValue = 0 }
+          answerArray[rowIndex][cellIndex] = cellValue
+        });
+      });
+      return answerArray
+    };
+
+    var $emptyBoardCells = $board.find('.cell.empty');
     $emptyBoardCells.click( function(e) {
-        if ($selected.length > 0) {
-          console.log($selected);
-          closeCellInput($selected);
-        }
         editCell($(this));
         return;
     });
+
+    var reset = function () {
+      $('.row').each(function (rowIndex, row) {
+        $(this).find('.cell').each(function (cellIndex, cell) {
+          var cellValue = expectedBoard[rowIndex][cellIndex];
+          if (cellValue) {
+            $(this).text(cellValue);
+            $(this).addClass('set');
+          }
+          else {
+            $(this).addClass('empty');
+          }
+        });
+      });
+    }
 
     function editCell($cell) {
       $cell.click(function() {
---
+++ app/sudoku.js
@@ -0,0 +1,62 @@
+var parseBoard = function(board) {
+  return board.split('\n').map(function(row) {
+    return row.split('').map(function(num) {
+      return +num;
+    });
+  });
+};
+
+var saveEmptyPositions = function(board) {
+  var emptyPositions = [];
+
+  for(var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      if(board[i][j] === 0) {
+        emptyPositions.push([i, j]);
+      }
+    }
+  }
+
+  return emptyPositions;
+};
+
+var checkRow = function(board, row, value) {
+  for(var i = 0; i < board[row].length; i++) {
+    if(board[row][i] === value) {
+      return [row, i];
+    }
+  }
+  return true;
+};
+
+var checkColumn = function(board, column, value) {
+  for(var i = 0; i < board.length; i++) {
+    if(board[i][column] === value) {
+      return [column, i];
+    }
+  }
+  return true;
+};
+
+var checkSquare = function(board, column, row, value) {
+  var columnCorner = 0,
+      rowCorner = 0,
+      squareSize = 3;
+
+      while(column >= columnCorner + squareSize) {
+    columnCorner += squareSize;
+  }
+
+  while(row >= rowCorner + squareSize) {
+    rowCorner += squareSize;
+  }
+
+  for(var i = rowCorner; i < rowCorner + squareSize; i++) {
+    for(var j = columnCorner; j < columnCorner + squareSize; j++) {
+      if(board[i][j] === value) {
+        return [i, j];
+      }
+    }
+  }
+  return true;
+};
