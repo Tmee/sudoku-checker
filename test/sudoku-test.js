69fc8383e3627609fe056e82f6632c11fef1dc89
Timothy Mee <tjmee90@gmail.com>
2015-02-19 14:11:04 -0700

    removed unused code from sudoku.js

--- test/sudoku-test.js
+++ test/sudoku-test.js
@@ -22,15 +22,6 @@ module ('Sudoku Puzzle Tests');
     [3,0,0,0,9,0,0,0,0]
   ];
 
-  var expectedPositions = [
-    [0,0],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[1,0],[1,1],
-    [1,2],[1,5],[2,0],[2,1],[2,2],[2,6],[2,7],[2,8],[3,0],
-    [3,1],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[4,4],[4,5],
-    [4,7],[4,8],[5,1],[5,3],[5,4],[5,5],[5,8],[6,1],[6,2],
-    [6,4],[6,6],[6,8],[7,1],[7,3],[7,4],[7,5],[7,8],[8,1],
-    [8,2],[8,3],[8,5],[8,6],[8,7],[8,8]
-  ];
-
 test('make board into an array', function() {
 
   var parsedBoard = parseBoard(board);
@@ -38,15 +29,6 @@ test('make board into an array', function() {
   deepEqual(9, parsedBoard.length);
   deepEqual(9, parsedBoard[0].length);
   deepEqual(parsedBoard, expectedBoard);
-
-});
-
-test('should save all of the empty positions of board', function() {
-
-  var emptyPositions = saveEmptyPositions(expectedBoard);
-
-  deepEqual(51, emptyPositions.length);
-  deepEqual(expectedPositions, emptyPositions);
 
 });
 
